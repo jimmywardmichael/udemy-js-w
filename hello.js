cb0adf5871e4ac766499dce03839afbe52026e3f
James Ward <jimmywardmichael@gmail.com>
2023-12-20 12:58:16 -0600

    capital challenge

--- hello.js
+++ hello.js
@@ -15,19 +15,28 @@
 
 
 //Strings and properties
-const s = new String('Hello World');
-x = typeof s;
-x = s.length;
+// const s = new String('Hello World');
+// x = typeof s;
+// x = s.length;
+//
+// //Access value by key
+// x = s[0];
+// x = s._proto_;
+//
+// x = s.toUpperCase();
+// x = s.toLowerCase();
+//
+// x = s.charAt(4)
+// x = s.substring(0,4);
+// x = s.substring(7)
+// x = s.replace('World', 'James');
+// x = s.includes('Hel');
+// x = s.split('');
+// console.log(x);
+const myString = 'developer';
+let myNewString;
 
-//Access value by key
-x = s[0];
-x = s._proto_;
+//solution 1 capital challenge
+myNewString = myString.charAt(0).toUpperCase() + myString.substring(1, 9);
 
-x = s.toUpperCase();
-x = s.toLowerCase();
-
-x = s.charAt(4)
-x = s.substring(0,4);
-x = s.substring(7)
-
-console.log(x);
+console.log(myNewString)
