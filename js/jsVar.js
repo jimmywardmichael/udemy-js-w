4a66be35b7938b9e32c56e767ccf353730b2f6ab
James Ward <jimmywardmichael@gmail.com>
2024-01-30 09:07:03 -0600

    undo stash

---
+++ js/jsVar.js
@@ -0,0 +1,3 @@
+var tweet = prompt("Compose your tweet:");
+// var tweetCount = tweet.length;
+alert("You have written " + tweet.length + " characters, you have " + (140 - tweet.length) + " characters remaining.");
