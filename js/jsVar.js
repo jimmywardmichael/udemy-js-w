169e7f1b577f677978be2425fab6d4cd02761464
James Ward <jimmywardmichael@gmail.com>
2024-01-30 09:38:17 -0600

    alert

--- js/jsVar.js
+++ js/jsVar.js
@@ -1,3 +1,13 @@
-var tweet = prompt("Compose your tweet:");
-// var tweetCount = tweet.length;
-alert("You have written " + tweet.length + " characters, you have " + (140 - tweet.length) + " characters remaining.");
+// var tweet = prompt("Compose your tweet:");
+// // var tweetCount = tweet.length;
+// alert("You have written " + tweet.length + " characters, you have " + (140 - tweet.length) + " characters remaining.");
+
+// alert(prompt("Compose your tweet:").slice(0,140));
+
+var name = prompt("What is your name?");
+
+var firstChar = name.slice(0,1);
+var upperCaseFirstChar = firstChar.toUpperCase();
+var restOfName = name.slice(1,name.length);
+var capitalisedName = upperCaseFirstChar + restOfName;
+alert("Hello, " + capitalisedName);
